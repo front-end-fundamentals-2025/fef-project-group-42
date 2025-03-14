729257a24fa42c7826c9aa5ad8b3a1792efa20f2
Arad190783 <noar24xe@student.ju.se>
2025-03-14 16:02:09 +0100

    final touches and the website is good to work with now!!

--- js/main.js
+++ js/main.js
@@ -143,3 +143,21 @@ function removeFromCart(index) {
   /* reload the cart display */
   loadCart();
 }
+
+/* sign in submittion */
+document
+  .querySelector(".signIn-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const form = document.querySelector(".signIn-form");
+    const message = document.getElementById("submittedMessage");
+    form.classList.add("fade-out");
+
+    setTimeout(() => {
+      form.style.display = "none";
+      message.style.display = "block";
+      setTimeout(() => {
+        message.classList.add("fade-in");
+      }, 10);
+    }, 500);
+  });
